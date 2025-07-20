cd5458a2509a01bc1d77dccf441803f79860651d
DennisGabriel <dennisgabr745@gmail.com>
2025-07-20 01:46:48 -0300

    create styles and configs firebase

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Meu primeiro blog em React!</h3>
+      <p>Mini blog &copy; 2025</p>
+    </footer>
+  )
+}
+
+export default Footer
