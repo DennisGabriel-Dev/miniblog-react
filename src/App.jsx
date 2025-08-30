4847fb854e1c2c3ebb4954763014ee78961a0c84
DennisGabriel <dennisgabr745@gmail.com>
2025-08-30 17:25:51 -0300

    login user

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+// context
+import { AuthProvider } from './context/AuthContext'
+
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
@@ -12,18 +15,20 @@ import Login from './pages/Login'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter >
-        <Navbar />
-        <div className='container'>
-            <Routes>
-              <Route path='/' element={<Home />}/>
-              <Route path='/about' element={<About />}/>
-              <Route path='/register' element={<Register />} />
-              <Route path='/login' element={<Login />} />
-            </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter >
+          <Navbar />
+          <div className='container'>
+              <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/about' element={<About />}/>
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+              </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -22,4 +22,53 @@ export const useAuthentication = () => {
       return
     }
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled()
+
+    setLoading(true)
+
+    try {
+      const { user } = createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+
+      await updateProfile(user, {
+        displayName: data.displayName
+      })
+
+      setLoading(false)
+      return user
+
+    } catch (error) {
+      console.log(error.message)
+      console.log(typeof error.message)
+      let messageError
+      if(error.message.includes("EMAIL_EXISTS")) {
+        messageError = "E-mail já cadastrado."
+      } else if (error.message.includes("invalid-email")) {
+        messageError = "E-mail inválido."
+      } else if (error.message.includes("password")) {
+        messageError = "A senha precisa ter no mínimo 6 caracteres."
+      } else {
+        messageError = "Ocorreu um erro, por favor tente mais tarde."
+      }
+      setError(messageError)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    return () => setCancelled(true)
+  })
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading
+  }
 }
